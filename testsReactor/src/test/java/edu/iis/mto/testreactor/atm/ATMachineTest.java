eaf56de402fa4d9d5da14fcf26ed2d5a6e289193
pawelwelfle <pawel.welfle@gmail.com>
2020-06-18 17:24:25 +0200

    Mock and Setup

--- testsReactor/src/test/java/edu/iis/mto/testreactor/atm/ATMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/atm/ATMachineTest.java
@@ -2,17 +2,27 @@ package edu.iis.mto.testreactor.atm;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import edu.iis.mto.testreactor.atm.bank.Bank;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Currency;
 
 @ExtendWith(MockitoExtension.class)
 public class ATMachineTest {
 
+    @Mock
+    private Bank bank;
+    private Currency currency;
+
     @BeforeEach
-    public void setUp() throws Exception {}
+    public void setUp() throws Exception {
+        ATMachine atMachine = new ATMachine(bank, currency);
+    }
 
     @Test
     public void itCompiles() {
